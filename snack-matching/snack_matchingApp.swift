695bccd3b4d56f4d4896e01f35d0084fbbbaf3ca
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-03 17:32:16 +0900

    Initial Commit

---
+++ snack-matching/snack_matchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  snack_matchingApp.swift
+//  snack-matching
+//
+//  Created by Hasegawa Akito on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct snack_matchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
