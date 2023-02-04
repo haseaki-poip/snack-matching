c618fde280c8dce63ad6168ee8b9715f8d7d91f7
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-05 01:42:44 +0900

    お菓子リストのViewModel作成

---
+++ snack-matching/Views/SwipeOkashi.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SwipeOkashi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwipeOkashi_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeOkashi()
+    }
+}
