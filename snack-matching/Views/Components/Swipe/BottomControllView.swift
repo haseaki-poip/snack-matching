e0e6e204cab12e6746301908e236eb36f2e4852f
Hasegawa-Akito <h.akito1999@gmail.com>
2023-02-05 18:25:16 +0900

    BottomButtonのリファクタリング

--- snack-matching/Views/Components/Swipe/BottomControllView.swift
+++ snack-matching/Views/Components/Swipe/BottomControllView.swift
@@ -11,11 +11,11 @@ struct BottomControllView: View {
     var body: some View {
         HStack {
             
-            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, frameWidth: frameWidth, imageSize: 30, backgroundSize: 65, imageName: "xmark", imageColor: Color(UIColor.systemPink), backgroundColor: Color(UIColor.white))
+            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, controllType: .dislike)
             
-            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, frameWidth: frameWidth, imageSize: 15, backgroundSize: 50, imageName: "questionmark", imageColor: Color(UIColor.white), backgroundColor: Color(UIColor.gray))
+            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, controllType: .detail)
             
-            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, frameWidth: frameWidth, imageSize: 30, backgroundSize: 65, imageName: "heart", imageColor: Color(UIColor.systemPink), backgroundColor: Color(UIColor.white))
+            BottomButtonView(okashiDatalist: okashiDatalist, swipeController: swipeController, controllType: .like)
                    
         }
         .padding()
@@ -24,38 +24,85 @@ struct BottomControllView: View {
 }
 
 struct BottomButtonView: View {
+    private let frameWidth: CGFloat = CGFloat(
+        UIScreen.main.bounds.width
+    )
+    
+    enum ControllType: Int {
+        case like, dislike, detail
+        
+        func imageName() -> String {
+                switch( self ){
+                        case .like:
+                            return "heart"
+                        case .dislike:
+                            return "xmark"
+                        case .detail:
+                            return "questionmark"
+                }
+        }
+    }
+    
     var okashiDatalist: OkashiData
     var swipeController: SwipeController
-    
-    let frameWidth: CGFloat
-    let imageSize: CGFloat
-    let backgroundSize: CGFloat
-    let imageName: String
-    let imageColor: Color
-    let backgroundColor: Color
+    let controllType: ControllType
     
     
     var body: some View {
         ZStack {
             
-            backgroundColor
-                .frame(width: backgroundSize, height: backgroundSize)
-                .cornerRadius(50)
-                .shadow(radius: 10)
+            if controllType == .detail {
+                Color(UIColor.gray)
+                    .asRoundShadow(size: 50)
+            }
+            else {
+                Color(UIColor.white)
+                    .asRoundShadow(size: 65)
+            }
             
             Button(action: {
-                swipeController.finishSwipe(startLocation: CGPoint(x: 0, y: 0), location: CGPoint(x: 151, y: 0), okashiDatalist: okashiDatalist)
+                
+                if controllType == .like {
+                    swipeController.finishSwipe(startLocation: CGPoint(x: 0, y: 0), location: CGPoint(x: 151, y: 0), okashiDatalist: okashiDatalist)
+                }
+                else if controllType == .dislike {
+                    swipeController.finishSwipe(startLocation: CGPoint(x: 0, y: 0), location: CGPoint(x: -151, y: 0), okashiDatalist: okashiDatalist)
+                }
+                else {
+                    print("detail")
+                }
                 
             }, label: {
-                Image(systemName: imageName)
+                Image(systemName: controllType.imageName())
                     .resizable()
                     .scaledToFill()
-                    .frame(width: imageSize, height: imageSize)
-                    .foregroundColor(imageColor)
+                    .frame(
+                        width: controllType == .detail ? 15 : 30,
+                        height: controllType == .detail ? 15 : 30
+                    )
+                    .foregroundColor(controllType == .detail ? Color(UIColor.white) : Color(UIColor.systemPink))
 
             })
                 
         }
         .frame(width: frameWidth / 4)
+    }
+}
+
+// modifireの共通化
+struct BackgroundModifire: ViewModifier {
+    let size: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: size, height: size)
+            .cornerRadius(50)
+            .shadow(radius: 10)
+    }
+}
+
+extension View {
+    func asRoundShadow(size: CGFloat) -> some View {
+        modifier(BackgroundModifire(size: size))
     }
 }
